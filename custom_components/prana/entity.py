1cebd081ee5ac229a85bc742d451c0a2d3d2cc05
corvis <corvis.mail@gmail.com>
2020-12-23 23:12:24 +0200

    Get rid of CoordinatorEntity to support previous versions of hass. Fixed bug related to seed parsing

--- custom_components/prana/entity.py
+++ custom_components/prana/entity.py
@@ -1,8 +1,10 @@
 from abc import ABCMeta
 
 from homeassistant.components.fan import FanEntity, SUPPORT_SET_SPEED
+from homeassistant.core import callback
+from homeassistant.helpers.device_registry import IDX_IDENTIFIERS
 from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from prana_rc.contrib.api import PranaStateDTO
 from prana_rc.contrib.client.common import PranaRCAsyncClient
 from typing import Optional, Dict, Any, Union
@@ -12,22 +14,55 @@ from . import const, utils
 PranaEntity = Union["BasePranaEntity", "PranaDependantEntity"]
 
 
-class BasePranaEntity(CoordinatorEntity):
+class BasePranaEntity(Entity):
     """Parent class for Prana entities"""
 
     def __init__(
         self,
-        coordinator,
+        coordinator: DataUpdateCoordinator,
         api_client: PranaRCAsyncClient,
         device_config: dict,
         base_entity_id: str,
         base_entity_name: str,
     ):
-        super().__init__(coordinator)
+        super().__init__()
+        self.coordinator = coordinator
         self._base_entity_id = base_entity_id
         self._base_entity_name = base_entity_name
         self._device_config = device_config
         self.api_client = api_client
+
+    @property
+    def should_poll(self) -> bool:
+        """No need to poll. Coordinator notifies entity of updates."""
+        return False
+
+    @property
+    def available(self) -> bool:
+        """Return if entity is available."""
+        return self.coordinator.last_update_success
+
+    async def async_added_to_hass(self) -> None:
+        """When entity is added to hass."""
+        await super().async_added_to_hass()
+        self.async_on_remove(self.coordinator.async_add_listener(self._handle_coordinator_update))
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        self.async_write_ha_state()
+
+    async def async_update(self) -> None:
+        """Update the entity.
+
+        Only used by the generic entity update service.
+        """
+
+        # Ignore manual update requests if the entity is disabled
+        if not self.enabled:
+            return
+
+        await self.coordinator.async_request_refresh()
 
     @property
     def _prana_state(self) -> PranaStateDTO:
@@ -40,6 +75,7 @@ class BasePranaEntity(CoordinatorEntity):
     @property
     def device_info(self) -> Optional[Dict[str, Any]]:
         return {
+            IDX_IDENTIFIERS: {(const.DOMAIN, self.unique_id)},
             const.ATTR_DEVICE_ADDRESS: self._device_config.get(const.OPT_DEVICE_ADDRESS),
             const.ATTR_DEVICE_NAME: self._device_config.get(const.OPT_DEVICE_NAME),
         }
--- custom_components/prana/fan.py
+++ custom_components/prana/fan.py
@@ -188,7 +188,7 @@ class PranaInputFan(PranaSupplementaryFan):
 
     @property
     def speed(self) -> Optional[str]:
-        return utils.speed_int_to_str(self.main_entity.state_attributes.get(const.ATTR_IN_SPEED, utils.PRANA_SPEEDS[0]))
+        return utils.speed_int_to_str(self.main_entity.state_attributes.get(const.ATTR_IN_SPEED, 0))
 
     async def async_turn_on(self, speed: Optional[str] = "2", **kwargs) -> None:
         pass  # Not supported yet
