1fa0093f694f029502220ec00f5a079dc7b58e7e
sergiu.popescu <sergiu.popescu@ailylabs.com>
2022-03-28 22:00:32 +0300

    Add all the sensors

--- custom_components/prana/sensor.py
+++ custom_components/prana/sensor.py
@@ -13,6 +13,11 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, asyn
 
     for prana_fan in hass.data[const.DOMAIN][config_entry.entry_id][const.DATA_MAIN_ENTITIES]:
         entities.append(PranaCO2(prana_fan))
+        entities.append(PranaVOC(prana_fan))
+        entities.append(PranaHumidity(prana_fan))
+        entities.append(PranaAirPressure(prana_fan))
+        entities.append(PranaAirTemperatureIn(prana_fan))
+        entities.append(PranaAirTemperatureOut(prana_fan))
 
     if len(entities) > 0:
         async_add_entities(entities)
@@ -34,6 +39,100 @@ class PranaCO2(PranaDependantEntity, SensorEntity):
 
     @property
     def native_value(self):
-        """Return the brightness of this light between 0..255."""
         prana_value = self.main_entity._prana_state.sensors.co2
+        return prana_value
+
+
+class PranaVOC(PranaDependantEntity, SensorEntity):
+
+    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_icon = "mdi:air-filter"
+
+    @property
+    def unique_id(self) -> Optional[str]:
+        return self.main_entity.unique_id + "_voc"
+
+    @property
+    def name(self) -> Optional[str]:
+        return self.main_entity.name + " VOC"
+
+    @property
+    def native_value(self):
+        prana_value = self.main_entity._prana_state.sensors.voc
+        return prana_value
+
+
+class PranaHumidity(PranaDependantEntity, SensorEntity):
+
+    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_icon = "mdi:water-percent"
+
+    @property
+    def unique_id(self) -> Optional[str]:
+        return self.main_entity.unique_id + "_humidity"
+
+    @property
+    def name(self) -> Optional[str]:
+        return self.main_entity.name + " Humidity"
+
+    @property
+    def native_value(self):
+        prana_value = self.main_entity._prana_state.sensors.humidity
+        return prana_value
+
+
+class PranaAirPressure(PranaDependantEntity, SensorEntity):
+
+    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_icon = "mdi:gauge"
+
+    @property
+    def unique_id(self) -> Optional[str]:
+        return self.main_entity.unique_id + "_pressure"
+
+    @property
+    def name(self) -> Optional[str]:
+        return self.main_entity.name + " Pressure"
+
+    @property
+    def native_value(self):
+        prana_value = self.main_entity._prana_state.sensors.pressure
+        return prana_value
+
+
+class PranaAirTemperatureIn(PranaDependantEntity, SensorEntity):
+
+    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_icon = "mdi:thermometer"
+
+    @property
+    def unique_id(self) -> Optional[str]:
+        return self.main_entity.unique_id + "_temperature_in"
+
+    @property
+    def name(self) -> Optional[str]:
+        return self.main_entity.name + " Temperature Out"
+
+    @property
+    def native_value(self):
+        prana_value = self.main_entity._prana_state.sensors.temperature_in
+        return prana_value
+
+
+class PranaAirTemperatureOut(PranaDependantEntity, SensorEntity):
+
+    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_icon = "mdi:thermometer"
+
+    @property
+    def unique_id(self) -> Optional[str]:
+        return self.main_entity.unique_id + "_temperature_out"
+
+    @property
+    def name(self) -> Optional[str]:
+        return self.main_entity.name + " Temperature Out"
+
+    @property
+    def native_value(self):
+        prana_value = self.main_entity._prana_state.sensors.temperature_out
         return prana_value
